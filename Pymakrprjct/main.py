82145da6cab7a132a9b048712a6f9234c761449b
Harm1990 <84412382+Harm1990@users.noreply.github.com>
2025-03-15 14:10:24 +0100

    Fixed GRB/RGB addressing of LED

--- Pymakrprjct/main.py
+++ Pymakrprjct/main.py
@@ -3,25 +3,29 @@ import machine, neopixel
 
 # Set up the NeoPixel debug strip
 np = neopixel.NeoPixel(machine.Pin(47), 1)
+np.ORDER = (0,1,2,3) # On Wemos S3 mini board, red and green are swapped
 brightness = 22
 # Set up the button
 button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
+previous_button_state = button.value()
 
 while True:
     if button.value() == 0:
         print("Button pressed!")
+        previous_button_state = button.value()
+        np[0] = (brightness,0,0) # Red
+        np.write()
+        time.sleep(1)
+        np[0] = (0,brightness,0) # Green
+        np.write()
+        time.sleep(1)
+        np[0] = (0,0,brightness) # Blue
+        np.write()
+        time.sleep(1)
     else:
-        print("Button not pressed!")
-    time.sleep(1)
-    np[0] = (0,brightness,0) # On Wemos board, red and green are swapped
-    np.write()
-    time.sleep(1)
-    np[0] = (brightness,0,0) # Red
-    np.write()
-    time.sleep(1)
-    np[0] = (0,0,brightness) # Blue
-    np.write()
-    time.sleep(1)
+        if previous_button_state != button.value():
+            print("Button not pressed!")
+            previous_button_state = button.value()
     np[0] = (0,0,0)
     np.write()
     time.sleep(1)   
